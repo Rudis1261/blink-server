d74e56a9a8cdb173ec1f9be324decc2bbc42e541
Charlie Bouthoorn <C-Bouthoorn@users.noreply.github.com>
2019-10-06 20:22:58 +0200

    Fixed duplicated left/right click

--- bluetooth_server.py
+++ bluetooth_server.py
@@ -30,16 +30,30 @@ print("READY FOR CONNECTIONS, RFCOMM channel %d" % port)
 while True:
     client_sock, client_info = server_sock.accept()
     print("INBOUND CONNECTION ", client_info)
+    
+    prevcmd = None
+    prevcmdtime = 0
+    
     try:
         while True:
             data = client_sock.recv(1024)
             if len(data) == 0: break
 
+            # Issue #6 where the left and right commands are duplicated
+            # within 500 ms, we'll ignore it
+            if data in ['left', 'right'] and data == prevcmd and time.time() - prevcmdtime < 0.5:
+                print("Blocked too fast double-click")
+                continue
+  
             print("COMMAND RECEIVED [%s]" % data)
             if data == "marco":
                 client_sock.send("polo\n")
             else:
                 commandKeys(data)
+                
+            prevcmd = data
+            prevcmdtime = time.time()
+
     except IOError:
         pass
 
